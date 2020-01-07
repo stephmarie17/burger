1d1c3b17888c9f3f20d10b17fa44da0b95c7804b
Stephanie A <stephanie.m.aurelio@gmail.com>
2020-01-07 10:10:31 -0800

    updated connection.js for heroku deploy

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-    host: "localhost",
+    host: "us-cdbr-iron-east-05.cleardb.net",
     port: 3306,
-    user: "root",
-    password: "",
-    database: "burgers_db"
+    user: "b17ee9350997c7",
+    password: "efba3f0d",
+    database: "heroku_6c7a099f15f256c"
 });
 
 connection.connect(function(err) {
