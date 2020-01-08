1dbdc7ef9d01bd6652f952953a819ec56e04903e
Stephanie A <stephanie.m.aurelio@gmail.com>
2020-01-07 20:14:20 -0800

    updated config

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     host: "us-cdbr-iron-east-05.cleardb.net",
     port: 3306,
     user: "b17ee9350997c7",
@@ -8,12 +8,12 @@ const connection = mysql.createConnection({
     database: "heroku_6c7a099f15f256c"
 });
 
-connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-    console.log("connected as id " + connection.threadId);
-  });
+// connection.connect(function(err) {
+//     if (err) {
+//       console.error("error connecting: " + err.stack);
+//       return;
+//     }
+//     console.log("connected as id " + connection.threadId);
+//   });
   
 module.exports = connection;
